e150cb8cb5f9b41cd7b025134cf923cfec5c3bda
pixelhijack <pothattila@gmail.com>
2016-09-13 20:54:05 +0000

    texture atlas added, sprite shown

--- dist/bundle.js
+++ dist/bundle.js
@@ -59,17 +59,24 @@
 	});
 	
 	function AsteroidAdventures(){
+	    var ship;
+	    
 	    this.init = function(config){
 	        console.log('[PHASER] init', config);
 	    };
 	    this.preload = function(){
 	        console.log('[PHASER] preload');
+	        this.game.load.atlas('ships', '../assets/ships.png', '../assets/ships.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 	    };
 	    this.create = function(){
 	        console.log('[PHASER] create');
+	        ship = this.game.add.sprite(100, 100, 'ships');
+	        ship.animations.add('idle', ['43'], 10, true);
+	        this.game.add.existing(ship);
 	    };
 	    this.update = function(){
 	        console.log('[PHASER] update');
+	        ship.animations.play('idle');
 	    };
 	}
 
