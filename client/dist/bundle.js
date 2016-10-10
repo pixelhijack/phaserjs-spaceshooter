cebfa6c271a851187e99a5ba312118a4f6b8d557
pixelhijack <pothattila@gmail.com>
2016-10-10 10:10:53 +0000

    unique id to sprites

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -361,7 +361,11 @@
 	};
 	
 	GameObject.prototype.setId = function(){
-	    this.id = this.constructor.name + '-' + (this.x | 0) + '-' + (this.y | 0);
+	    this.id = this.constructor.name + '-' +
+	        (this.x | 0) + '-' +
+	        (this.y | 0) + '-' +
+	        Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+	
 	};
 	
 	GameObject.prototype.debug = function(toDebug){
--- client/src/gameobject.js
+++ client/src/gameobject.js
@@ -72,7 +72,11 @@ GameObject.prototype.update = function(){
 };
 
 GameObject.prototype.setId = function(){
-    this.id = this.constructor.name + '-' + (this.x | 0) + '-' + (this.y | 0);
+    this.id = this.constructor.name + '-' +
+        (this.x | 0) + '-' +
+        (this.y | 0) + '-' +
+        Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+
 };
 
 GameObject.prototype.debug = function(toDebug){
